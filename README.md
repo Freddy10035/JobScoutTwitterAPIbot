b7f7e69661e84996e45135b6c1b0913e040a6a7c
Fredrick Ochieng <fredrick_ochieng@outlook.com>
2023-05-18 08:32:18 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+**Description:**
+
+The JobScoutTwitterAPIbot is a Python script that automates the process of posting job openings to Twitter. It fetches job data from a custom API, constructs tweets with relevant job information, and posts them to a Twitter account using the Tweepy library. The bot also includes functionality to shorten URLs using the TinyURL API to fit within Twitter's character limit.
+
+**Features:**
+- Retrieves job data from a custom API using authentication.
+- Constructs informative tweets with job details including job title, company name, location, job function, qualifications, salary, and experience level.
+- Shortens job URLs using the TinyURL API to fit within Twitter's character limit.
+- Posts job tweets to a Twitter account using the Tweepy library.
+- Handles errors gracefully and displays appropriate error messages.
+
+**Installation:**
+
+1. Clone the repository or download the script files.
+2. Install the required dependencies by running `pip install -r requirements.txt`.
+3. Set up the necessary environment variables in a `.env` file or your preferred method. Required variables include:
+   - `CONSUMER_KEY`: Twitter API consumer key.
+   - `CONSUMER_SECRET`: Twitter API consumer secret.
+   - `ACCESS_TOKEN`: Twitter API access token.
+   - `ACCESS_TOKEN_SECRET`: Twitter API access token secret.
+   - `TINY_URL_API_TOKEN`: Token for accessing the TinyURL API.
+   - `ALL_JOBS_API_BEARER_TOKEN`: Bearer token for API authentication.
+4. Customize the `job_api_endpoint` variable to point to your desired job data API endpoint.
+5. Run the script using `python tweetScript.py`.
+
+**Usage:**
+
+1. Ensure the required environment variables are correctly set with valid credentials.
+2. Customize the `job_api_endpoint` variable to point to your desired job data API endpoint.
+3. Adjust the `tweet_limit` variable to control the number of tweets to post.
+4. Run the script using `python tweetScript.py`.
+5. The bot will fetch job data, construct tweets, and post them to the specified Twitter account.
+
+**Contributing:**
+
+Contributions to the JobScoutTwitterAPIbot are welcome! If you encounter any issues, have suggestions for improvements, or would like to add new features, please feel free to submit a pull request.
+
---
+++ tweetScript.py
@@ -0,0 +1,96 @@
+import os
+import time
+import requests
+from dotenv import load_dotenv
+from tweepy import Client
+
+load_dotenv()
+
+# Twitter API credentials
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+client = Client(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret
+)
+
+# API endpoint for retrieving job data
+job_api_endpoint = 'http://localhost/all-jobs-api/api/v1/jobs/'
+
+# Bearer token for API authentication
+bearer_token = os.getenv("ALL_JOBS_API_BEARER_TOKEN")
+
+# Set the authorization header
+job_headers = {'Authorization': f'Bearer {bearer_token}'}
+
+# Fetch job data from the API with authentication
+job_response = requests.get(job_api_endpoint, headers=job_headers)
+jobs = job_response.json()
+# print(jobs)
+
+# Limit the number of tweets
+tweet_limit = 5
+tweet_count = 0
+
+
+# Function to shorten URLs using the TinyURL API
+def shorten_url(url):
+    tiny_url_api_token = os.getenv("TINY_URL_API_TOKEN")
+    tiny_url_endpoint = "https://api.tinyurl.com/create"
+    tiny_url_headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {tiny_url_api_token}"
+    }
+    tiny_url_payload = {
+        "url": url
+    }
+
+    tiny_url_response = requests.post(tiny_url_endpoint, headers=tiny_url_headers, json=tiny_url_payload)
+    tiny_url_response_json = tiny_url_response.json()
+    shortened_url = tiny_url_response_json['data']['tiny_url']
+    # print(shortened_url)  # Print the response for debugging purposes
+    return shortened_url  # Replace with the correct short URL retrieval logic
+
+
+# Loop through each job and post it to Twitter
+for job in jobs['data']:
+    job_title = job['job_title']
+    company_name = job['company_name']
+    job_location = job['job_location']
+    job_function = job['job_function']
+    min_qualifications = job['min_qualifications']
+    job_salary = job['job_salary']
+    experience_length = job['experience_length']
+    job_link = job['job_link']
+
+    # Shorten the job link
+    short_job_link = shorten_url(job_link)
+
+    # Construct the tweet message
+    tweet = f"🌟 Job Alert: {job_title}\n\n🏢 {company_name} is hiring!\n\n📍 Location: {job_location}\n🔧 Category: {job_function}\n🎓 Qualifications: {min_qualifications}\n💰 Salary: {job_salary}\n⏳ Experience: {experience_length}\n\n🔗 Apply here: {short_job_link}"
+
+    # Check if the tweet exceeds the character limit
+    if len(tweet) > 280:
+        print("Tweet is too long and cannot be posted as a single tweet.")
+        continue
+
+    # Post the tweet
+    try:
+        client.create_tweet(text=tweet)
+        print("Tweet posted successfully:\n", tweet)
+        tweet_count += 1
+        time.sleep(1)
+    except Exception as e:
+        print("An error occurred while posting the tweet:", str(e))
+
+    # Check if the tweet limit has been reached
+    if tweet_count >= tweet_limit:
+        break
+
+print(f'{tweet_count} tweets posted successfully!')
