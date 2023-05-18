a7e378a3322e221b722751d9c4aad67ee5800193
Fredrick Ochieng <fredrick_ochieng@outlook.com>
2023-05-18 10:03:53 +0300

    more logic incoming

--- tweetScript.py
+++ tweetScript.py
@@ -34,7 +34,7 @@ jobs = job_response.json()
 # print(jobs)
 
 # Limit the number of tweets
-tweet_limit = 5
+tweet_limit = 30
 tweet_count = 0
 
 
@@ -58,6 +58,17 @@ def shorten_url(url):
     return shortened_url  # Replace with the correct short URL retrieval logic
 
 
+# Define the categories you want to post jobs for
+categories_to_post = ["Accounting, Auditing & Finance", "Software & Data",
+                      "Engineering & Technology", "Marketing & Communications",
+                      "Creative & Design", "Research, Teaching & Training"]
+
+# Define the maximum number of jobs per category
+max_jobs_per_category = 5
+
+# Initialize a dictionary to store the count of jobs posted for each category
+jobs_per_category = {category: 0 for category in categories_to_post}
+
 # Loop through each job and post it to Twitter
 for job in jobs['data']:
     job_title = job['job_title']
@@ -69,28 +80,35 @@ for job in jobs['data']:
     experience_length = job['experience_length']
     job_link = job['job_link']
 
-    # Shorten the job link
-    short_job_link = shorten_url(job_link)
+    # Check if the job's category is in the categories_to_post list
+    if job_function in categories_to_post:
 
-    # Construct the tweet message
-    tweet = f"🌟 Job Alert: {job_title}\n\n🏢 {company_name} is hiring!\n\n📍 Location: {job_location}\n🔧 Category: {job_function}\n🎓 Qualifications: {min_qualifications}\n💰 Salary: {job_salary}\n⏳ Experience: {experience_length}\n\n🔗 Apply here: {short_job_link}"
+        # Check if the maximum number of jobs per category has been reached
+        if jobs_per_category[job_function] >= max_jobs_per_category:
+            continue
 
-    # Check if the tweet exceeds the character limit
-    if len(tweet) > 280:
-        print("Tweet is too long and cannot be posted as a single tweet.")
-        continue
+        # Shorten the job link
+        short_job_link = shorten_url(job_link)
 
-    # Post the tweet
-    try:
-        client.create_tweet(text=tweet)
-        print("Tweet posted successfully:\n", tweet)
-        tweet_count += 1
-        time.sleep(1)
-    except Exception as e:
-        print("An error occurred while posting the tweet:", str(e))
+        # Construct the tweet message
+        tweet = f"🌟 Job Alert: {job_title}\n\n🏢 {company_name} is hiring!\n\n📍 Location: {job_location}\n🔧 Category: {job_function}\n🎓 Qualifications: {min_qualifications}\n💰 Salary: {job_salary}\n⏳ Experience: {experience_length}\n\n🔗 Apply here: {short_job_link}"
 
-    # Check if the tweet limit has been reached
-    if tweet_count >= tweet_limit:
-        break
+        # Check if the tweet exceeds the character limit
+        if len(tweet) > 280:
+            print("Tweet is too long and cannot be posted as a single tweet.")
+            continue
+
+        # Post the tweet
+        try:
+            client.create_tweet(text=tweet)
+            print("Tweet posted successfully:\n", tweet)
+            tweet_count += 1
+            time.sleep(5)
+        except Exception as e:
+            print("An error occurred while posting the tweet:", str(e))
+
+        # Check if the tweet limit has been reached
+        if tweet_count >= tweet_limit:
+            break
 
 print(f'{tweet_count} tweets posted successfully!')
